4ea321d9517239166f491f7b716162762755c822
davenkin <davenkin@gmail.com>
2013-03-19 23:47:55 +0800

    config refactor

--- src/main/java/davenkin/DefaultOrderService.java
+++ src/main/java/davenkin/DefaultOrderService.java
@@ -6,15 +6,6 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.Serializable;
-
-/**
- * Created with IntelliJ IDEA.
- * User: davenkin
- * Date: 3/14/13
- * Time: 9:28 PM
- * To change this template use File | Settings | File Templates.
- */
 public class DefaultOrderService  implements OrderService{
     private JmsTemplate jmsTemplate;
     private SessionFactory sessionFactory;
